23e01fe7609b366c93d1b76f4e1b4186c944ffbb
Achraf Atila <achraf.atila@gmail.com>
2026-08-24 01:43:04 +0200

    Revert "add spec-engine"

--- conftest.py
+++
@@ -1,8 +0,0 @@
-"""Pytest configuration: render headlessly so tests and doctests never open a window.
-
-Author: Achraf Atila (achraf.atila@bam.de)
-"""
-
-import matplotlib
-
-matplotlib.use("Agg")
